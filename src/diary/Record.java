69eb6c45b133fe4217c55b27f6bcc28ee58d074b
LaunDan <launadaniel2@gmail.com>
2020-03-06 19:41:04 +0100

    description od diary

--- src/diary/Record.java
+++ src/diary/Record.java
@@ -2,29 +2,51 @@ package diary;
 
 import java.time.LocalDateTime;
 
+/**
+ * Class present record of diary
+ */
 public class Record {
     private LocalDateTime dateTime;
     private String text;
 
+    /**
+     * class presents record of diary
+     * @param dateTime date and time
+     * @param text text od record
+     */
     public Record(LocalDateTime dateTime, String text){
         this.dateTime = dateTime;
         this.text = text;
     }
+
     /**
-     *
+     * @return date and time
      */
     public LocalDateTime getDateTime(){
         return dateTime;
     }
+
+    /**
+     * @param dateTime the date time to set
+     */
     public void setDateTime(LocalDateTime dateTime){
          this.dateTime = dateTime;
     }
+
+    /**
+     * @return the text
+     */
     public String getText(){
         return text;
     }
+
+    /**
+     * @param text text to set
+     */
     public void setText(String text){
         this.text = text;
     }
+
     @Override
     public String toString(){
         return dateTime + " " + text;
