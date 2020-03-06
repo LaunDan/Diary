ed846f7399030c66459a4b924c4331e8fc52388f
LaunDan <launadaniel2@gmail.com>
2020-03-06 19:07:12 +0100

    clear main
    create class Record

--- src/diary/Main.java
+++ src/diary/Main.java
@@ -3,10 +3,6 @@ package diary;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-	System.out.println("blabla");
-=======
-	// write your code here
->>>>>>> f7b0efb... Create project
+
     }
 }
