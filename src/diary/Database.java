18309240dcb9df3633a10f9cd5d4b7ac15924795
LaunDan <launadaniel2@gmail.com>
2020-03-07 08:06:47 +0100

    create class Diary
    format and description of Database

--- src/diary/Database.java
+++ src/diary/Database.java
@@ -3,27 +3,54 @@ package diary;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+/**
+ * Class presents database of diary
+ */
 public class Database {
     private ArrayList<Record> records;
-    public Database(){
+
+    /**
+     * Create ArrayList records
+     */
+    public Database() {
         records = new ArrayList<>();
     }
-    public void addRecord(LocalDateTime dateTime, String text){
+
+    /**
+     * add record to the records
+     *
+     * @param dateTime date and time of records
+     * @param text     the text of record
+     */
+    public void addRecord(LocalDateTime dateTime, String text) {
         records.add(new Record(dateTime, text));
     }
 
-    public ArrayList<Record> findRecords(LocalDateTime date, boolean byTime){
+    /**
+     * Find record in records and return found records
+     *
+     * @param date   date of record
+     * @param byTime True/False for finding by time
+     * @return found records
+     */
+    public ArrayList<Record> findRecords(LocalDateTime date, boolean byTime) {
         ArrayList<Record> found = new ArrayList<>();
-        for (Record z : records){
-            if ((byTime && z.getDateTime().equals(date)) || (!byTime && z.getDateTime().toLocalDate().equals(date.toLocalDate()))){
+        for (Record z : records) {
+            if ((byTime && z.getDateTime().equals(date)) || (!byTime && z.getDateTime().toLocalDate().equals(date.toLocalDate()))) {
                 found.add(z);
             }
         }
         return found;
     }
-    public void deleteRecords(LocalDateTime date){
+
+    /**
+     * delete record by date
+     *
+     * @param date date of record which will be deleted
+     */
+    public void deleteRecords(LocalDateTime date) {
         ArrayList<Record> foundd = findRecords(date, true);
-        for (Record z : foundd){
+        for (Record z : foundd) {
             records.remove(z);
         }
     }
