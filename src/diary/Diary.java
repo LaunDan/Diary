3abc5d8359a66db2712f334f892fbafb0b9e03b9
LaunDan <launadaniel2@gmail.com>
2020-03-07 09:26:21 +0100

    create in diary enter time for find records

--- src/diary/Diary.java
+++ src/diary/Diary.java
@@ -1,5 +1,35 @@
 package diary;
 
-public class Diary {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 
+public class Diary {
+    private Database database;
+    private Scanner sc = new Scanner(System.in, "Windows-1250");
+
+    public Diary() {
+        database = new Database();
+    }
+
+    private LocalDateTime findOutDateTime(){
+        System.out.println("Enter date and time in format [dd.mm.yyyy hh:mm]:");
+        while (true){
+            try {
+                return LocalDateTime.parse(sc.nextLine(), FORMAT_DATA);
+            } catch (Exception ex){
+                System.out.println("Wrong entered, please enter again");
+            }
+        }
+    }
+    private LocalDateTime findOutDate(){
+        System.out.println("Enter date in format [dd.mm.yyyy]:");
+        while (true){
+            try {
+                return LocalDate.parse(sc.nextLine(), FORMAT_DATA_WITHOUT_TIME).atStartOfDay();
+            } catch (Exception ex){
+                System.out.println("Wrong entered, please enter again");
+            }
+        }
+    }
 }
